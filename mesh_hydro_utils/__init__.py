f25408d74e45547eb45afaa97f0aabc063c925b6
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2024-01-06 12:20:08 +0100

    initial commit module files

---
+++ mesh_hydro_utils/__init__.py
@@ -0,0 +1,10 @@
+"""
+
+"""
+
+from mesh_hydro_utils.__version__ import __version__
+from mesh_hydro_io import write_ic, read_output, read_ic
+
+from mesh_hydro_plotting import plot_1D, plot_1D_density_only, plot_2D, plot_2D_density_only, plot_2D_velorm, plot_2D_in_3D, plot_savefig, plot_get_figname
+from mesh_hydro_riemann import riemann_solver
+from mesh_hydro_utilities
