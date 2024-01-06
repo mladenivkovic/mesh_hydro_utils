709e7837caaee11f0c6fc3fa7079e7a7f6aa2651
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2024-01-06 14:03:55 +0100

    added misc dir

--- testing/run.sh
+++ testing/run.sh
@@ -3,64 +3,90 @@
 # exit on error
 set -e
 
+
+# ======================================
+# Setup
+# ======================================
+
 # turn sets of tests on and off
-test_ICs=true
+# test_ICs=true
+test_misc=true
+
+SCRIPTDIR="../scripts"
+
+
+# ======================================
+# IC tests
+# ======================================
 
 if [[ "$test_ICs" == "true" ]]; then
 
-    echo running ../scripts/IC/advection-1D-four-shapes.py
-    python3 ../scripts/IC/advection-1D-four-shapes.py
+    echo running $SCRIPTDIR/IC/advection-1D-four-shapes.py
+    python3 $SCRIPTDIR/IC/advection-1D-four-shapes.py
     rm advection-1D-four-shapes-256.dat
 
-    echo running ../scripts/IC/advection-1D-gaussian.py
-    python3 ../scripts/IC/advection-1D-gaussian.py
+    echo running $SCRIPTDIR/IC/advection-1D-gaussian.py
+    python3 $SCRIPTDIR/IC/advection-1D-gaussian.py
 	rm advection-1D-gaussian-256.dat
 
-    echo running ../scripts/IC/advection-1D-step.py
-    python3 ../scripts/IC/advection-1D-step.py
+    echo running $SCRIPTDIR/IC/advection-1D-step.py
+    python3 $SCRIPTDIR/IC/advection-1D-step.py
 	rm advection-1D-step-256.dat
 
-    echo running ../scripts/IC/advection-2D-four-shapes.py
-    python3 ../scripts/IC/advection-2D-four-shapes.py
+    echo running $SCRIPTDIR/IC/advection-2D-four-shapes.py
+    python3 $SCRIPTDIR/IC/advection-2D-four-shapes.py
 	rm advection-2D-four-shapes.dat
 
-    echo running ../scripts/IC/advection-2D-step.py
-    python3 ../scripts/IC/advection-2D-step.py
+    echo running $SCRIPTDIR/IC/advection-2D-step.py
+    python3 $SCRIPTDIR/IC/advection-2D-step.py
 	rm advection-2D-step-100.dat
 
-    echo running ../scripts/IC/center-explosion.py
-    python3 ../scripts/IC/center-explosion.py
+    echo running $SCRIPTDIR/IC/center-explosion.py
+    python3 $SCRIPTDIR/IC/center-explosion.py
 	rm center-explosion-200.dat
 
-    echo running ../scripts/IC/corner-explosion.py
-    python3 ../scripts/IC/corner-explosion.py
+    echo running $SCRIPTDIR/IC/corner-explosion.py
+    python3 $SCRIPTDIR/IC/corner-explosion.py
 	rm corner-explosion-200.dat
 
-    echo running ../scripts/IC/kelvin-helmholtz.py
-    python3 ../scripts/IC/kelvin-helmholtz.py
+    echo running $SCRIPTDIR/IC/kelvin-helmholtz.py
+    python3 $SCRIPTDIR/IC/kelvin-helmholtz.py
 	rm kelvin-helmholtz-256.dat
 
-    echo running ../scripts/IC/output_to_IC.py advection-2D-0004.out
-    python3 ../scripts/IC/output_to_IC.py advection-2D-0004.out
+    echo running $SCRIPTDIR/IC/output_to_IC.py advection-2D-0004.out
+    python3 $SCRIPTDIR/IC/output_to_IC.py advection-2D-0004.out
     rm "restart.dat"
 
-    echo running ../scripts/IC/rayleigh-taylor.py
-    python3 ../scripts/IC/rayleigh-taylor.py
+    echo running $SCRIPTDIR/IC/rayleigh-taylor.py
+    python3 $SCRIPTDIR/IC/rayleigh-taylor.py
 	rm rayleigh-taylor-512.dat
 
-    echo running ../scripts/IC/uniform-1D.py
-    python3 ../scripts/IC/uniform-1D.py
+    echo running $SCRIPTDIR/IC/uniform-1D.py
+    python3 $SCRIPTDIR/IC/uniform-1D.py
     rm uniform-1D-100.dat
 
-    echo running ../scripts/IC/uniform-2D.py
-    python3 ../scripts/IC/uniform-2D.py
+    echo running $SCRIPTDIR/IC/uniform-2D.py
+    python3 $SCRIPTDIR/IC/uniform-2D.py
     rm uniform-2D-100.dat
 
 else
     echo "Skipping IC script tests."
 fi
 
-exit
+
+
+# ======================================
+# Misc tests
+# ======================================
+
+if [[ "$test_misc" == "true" ]]; then
+    echo running $SCRIPTDIR/misc/convert_to_hdf5.py
+    python3 $SCRIPTDIR/misc/convert_to_hdf5.py advection-2D-0004.out
+    rm advection-2D-0004.hdf5
+else
+    echo "Skipping misc/ scripts."
+fi
+
 
 
 
